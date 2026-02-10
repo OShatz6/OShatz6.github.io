dd1d68b00d56a9b76ec522177d7c57becc43f848
OShatz6 <o.hydroxide@yandex.ru>
2026-02-10 10:23:26 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('.motto-btn');
+    button.addEventListener('click', function () {
+    alert('Money cannot buy happiness');
+});
+});
